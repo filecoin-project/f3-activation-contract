dca020f380cc14ae9aa8ef7d1be067c4e6dd76dc
Jakub Sztandera <oss@kubuxu.com>
2025-01-28 17:50:19 +0100

    feat: Add mine function to F3Parameters test file
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>

--- test/F3Parameters.js
+++ test/F3Parameters.js
@@ -1,6 +1,7 @@
 const {
   time,
   loadFixture,
+  mine,
 } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
