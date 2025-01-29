bf77ab5fa52355cfedcae02904ff5501107ed82e
Jakub Sztandera <oss@kubuxu.com>
2025-01-29 15:46:07 +0100

    feat: Update Hardhat config with new Solidity version and networks
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,36 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.23",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+        details: { yul: false },
+      },
+    },
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    localnet: {
+      chainId: 31415926,
+      url: "http://127.0.0.1:1234/rpc/v1",
+      accounts: [PRIVATE_KEY],
+    },
+    calibrationnet: {
+      chainId: 314159,
+      url: "https://api.calibration.node.glif.io/rpc/v1",
+      accounts: [PRIVATE_KEY],
+    },
+    filecoinmainnet: {
+      chainId: 314,
+      url: "https://api.node.glif.io",
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
