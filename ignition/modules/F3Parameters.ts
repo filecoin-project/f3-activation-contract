352991200228ebeb57f5bc2d1ae1e6700fe615f7
Steve Loeppky <stvn@loeppky.com>
2025-01-29 13:11:02 -0800

    Small contract cleanups when reading through the code and configuration

--- ignition/modules/F3Parameters.ts
+++ ignition/modules/F3Parameters.ts
@@ -1,12 +1,12 @@
 
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const JAN_1ST_2030 = 1893456000;
+const JAN_1ST_2030_EPOCH_SECONDS = Math.floor(new Date('2030-01-01T00:00:00Z').getTime() / 1000);
 const ONE_GWEI = 1_000_000_000n;
 
 export default buildModule("F3ParametersModule", (m) => {
   const owner = m.getParameter("owner", m.getAccount(0));
-  const expiration = m.getParameter("expirationTime", JAN_1ST_2030);
+  const expiration = m.getParameter("expirationTime", JAN_1ST_2030_EPOCH_SECONDS);
 
   const f3params = m.contract("F3Parameters", [owner, expiration]);
   return { f3params };
