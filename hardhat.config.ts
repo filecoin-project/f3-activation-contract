56cd885c5080c219ecf85721e7c5304b1d8b9ea0
Jakub Sztandera <oss@kubuxu.com>
2025-01-29 15:46:39 +0100

    feat: Import PRIVATE_KEY from .env and define it as a constant
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
 
 const config: HardhatUserConfig = {
