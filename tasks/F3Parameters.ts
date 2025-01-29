8f412ceb2fadb5648366b41037b2f5ba5990eece
Jakub Sztandera <oss@kubuxu.com>
2025-01-29 15:47:44 +0100

    feat: Add F3Parameters task file
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>

---
+++ tasks/F3Parameters.ts
@@ -0,0 +1,30 @@
+const { task } = require("hardhat/config");
+
+task("transferOwnership", "Transfers ownership of the contract to a new account")
+  .addParam("contract", "The address of the contract")
+  .addParam("newowner", "The address of the new owner")
+  .setAction(async (taskArgs, hre) => {
+    const contractAddress = taskArgs.contract;
+    const newOwnerAddress = taskArgs.newowner;
+
+    const F3Parameters = await hre.ethers.getContractFactory("F3Parameters");
+    const contract = F3Parameters.attach(contractAddress);
+
+    console.log(`Transferring ownership of contract at ${contractAddress} to ${newOwnerAddress}...`);
+    const tx = await contract.transferOwnership(newOwnerAddress);
+    await tx.wait();
+
+    console.log(`Ownership transferred to ${newOwnerAddress}`);
+  });
+
+task("queryOwnership", "Queries the current owner of the contract")
+  .addParam("contract", "The address of the contract")
+  .setAction(async (taskArgs, hre) => {
+    const contractAddress = taskArgs.contract;
+
+    const F3Parameters = await hre.ethers.getContractFactory("F3Parameters");
+    const contract = F3Parameters.attach(contractAddress);
+
+    const ownerAddress = await contract.owner();
+    console.log(`The current owner of the contract at ${contractAddress} is ${ownerAddress}`);
+  });
