1a34f7350eb722495193844de7afba47576a40aa
Jakub Sztandera <oss@kubuxu.com>
2025-01-29 15:42:50 +0100

    feat: Convert F3Parameters.js to TypeScript with type annotations
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>

--- ignition/modules/F3Parameters.js
+++ ignition/modules/F3Parameters.js
@@ -1,12 +1,12 @@
 
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+import { buildModule, ModuleBuilder } from "@nomicfoundation/hardhat-ignition/modules";
 
-const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI = 1_000_000_000n;
+const JAN_1ST_2030: number = 1893456000;
+const ONE_GWEI: bigint = 1_000_000_000n;
 
-module.exports = buildModule("F3ParametersModule", (m) => {
-  const owner = m.getParameter("owner", m.getAccount(0))
-  const expiration = m.getParameter("expiratinTime", JAN_1ST_2030)
+export default buildModule("F3ParametersModule", (m: ModuleBuilder) => {
+  const owner = m.getParameter<string>("owner", m.getAccount(0));
+  const expiration = m.getParameter<number>("expirationTime", JAN_1ST_2030);
 
   const f3params = m.contract("F3Parameters", [owner, expiration]);
   return { f3params };
