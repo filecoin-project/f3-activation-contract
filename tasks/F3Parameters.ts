f3271114df6aa18343d31c211a9766df7b503a5b
Jakub Sztandera <oss@kubuxu.com>
2025-01-30 15:27:50 +0100

    feat: Add tasks to fetch and set activation information with JSON support
    Signed-off-by: Jakub Sztandera <oss@kubuxu.com>

--- tasks/F3Parameters.ts
+++ tasks/F3Parameters.ts
@@ -1,5 +1,7 @@
 import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import fs from "fs";
+import zlib from "zlib";
 
 task("transferOwnership", "Transfers ownership of the contract to a new account")
   .addParam("contract", "The address of the contract")
@@ -16,6 +18,43 @@ task("transferOwnership", "Transfers ownership of the contract to a new account"
     await tx.wait();
 
     console.log(`Ownership transferred to ${newOwnerAddress}`);
+  });
+
+task("fetchActivationInformation", "Fetches the activation information from the contract")
+  .addParam("contract", "The address of the contract")
+  .setAction(async (taskArgs: { contract: string }, hre: HardhatRuntimeEnvironment) => {
+    const contractAddress = taskArgs.contract;
+
+    const F3Parameters = await hre.ethers.getContractFactory("F3Parameters");
+    const contract = F3Parameters.attach(contractAddress);
+
+    const [activationEpoch, manifestData] = await contract.activationInformation();
+    const jsonData = zlib.inflateSync(Buffer.from(manifestData)).toString();
+
+    console.log(`Activation Epoch: ${activationEpoch}`);
+    console.log(`Manifest Data: ${jsonData}`);
+  });
+
+task("setActivationInformation", "Sets the activation information on the contract")
+  .addParam("contract", "The address of the contract")
+  .addParam("filepath", "The path to the JSON file containing the manifest data")
+  .addParam("activationepoch", "The new activation epoch block number")
+  .setAction(async (taskArgs: { contract: string; filepath: string; activationepoch: string }, hre: HardhatRuntimeEnvironment) => {
+    const contractAddress = taskArgs.contract;
+    const filePath = taskArgs.filepath;
+    const activationEpoch = parseInt(taskArgs.activationepoch, 10);
+
+    const F3Parameters = await hre.ethers.getContractFactory("F3Parameters");
+    const contract = F3Parameters.attach(contractAddress);
+
+    const jsonData = fs.readFileSync(filePath, "utf8");
+    const manifestData = zlib.deflateSync(Buffer.from(jsonData));
+
+    console.log(`Setting activation information on contract at ${contractAddress}...`);
+    const tx = await contract.updateActivationInformation(activationEpoch, manifestData);
+    await tx.wait();
+
+    console.log(`Activation information set with epoch ${activationEpoch}`);
   });
 
 task("queryOwnership", "Queries the current owner of the contract")
